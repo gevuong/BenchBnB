d5094631b3ee2a38d708eb45da0df7d66893dd7f
George Vuong <georgevuong18@gmail.com>
2017-07-17 12:13:26 -0700

    first render setup test complete, and backend auth in progress

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  #associations?
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token = User.generate_session_token
+  end
+end
