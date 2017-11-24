9ad7c5cacf55ab3d625c4fcf6f57c3220c806d39
George V <georgevuong18@gmail.com>
2017-11-23 19:42:10 -0800

    add gitignore and delete frontend files

--- FEATURES.md
+++
@@ -1,8 +0,0 @@
-# Feature 1
-Displays all SFMUNI and ACTRANSIT busses moving in real-time.
-
-# Feature 2
-
-
-# Feature 3
-Bus is
--- transitninja/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import MapReducer from './map_reducer';
-
-export default combineReducers({
-  map: MapReducer,
-});
