0a34b59c04d1fd15f3a96d6282d1338157afe25b
George V <georgevuong18@gmail.com>
2017-11-23 20:59:57 -0800

    update MONGODB_URI and PROD_MONGODB to fix heroku deployment of transitninja api

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 // setup config folder to separate config connection from everything else. This allows you to easily change configuration to development config or production config.
 var configValues = require('./config');
 
-
 // returns connection string to the DB to connect to Mongo via Mongoose. You can pass in an option as an arg to determine if you want dev or prod, and return proper connection string.
 module.exports = {
   getDBConnectionString: function() {
@@ -9,3 +8,4 @@ module.exports = {
   }
 };
 
+// heroku config:set PROD_MONGODB=mongodb://guest:guest@ds251845.mlab.com:51845/public_transit_systems
